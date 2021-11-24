535333bcd9e5ac3256465bdc93be337b8dda2ad6
Brian McEachern <brianmc29@ufl.edu>
2021-11-24 13:44:39 -0500

    SDF

--- js/main.js
+++ js/main.js
@@ -1,37 +1,35 @@
 //function to start the Leaflet map
-function createMap(){
+function createMap() {
 
     //Varibles needed for running the funcation
-    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' +' <a href="http://mapbox.com">Mapbox</a>';
-    
+    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a>| '+'<a href="http://mapbox.com">Mapbox</a>';
     var apitoken = 'pk.eyJ1IjoiYnJpYW5tYzI5IiwiYSI6ImNrdm5vYWxwbmUwZTAycG1hM251dDZ1N3gifQ.yxAx6olipNKQCFJ_1hj0eA' //Enter your API Token - go to 'https://www.mapbox.com/install/' to yours */
     
-    var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}';
-    
+      
+    //URL used for Custom MapBox Style
     var mbStyleUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}';
     
     
     //For Custome basemaps - Replace your username and StyleID
-    var customeBasemap = L.tileLayer(mbStyleUrl, {id: 'brianmc29/ckvnoe41y7ebt15pelxmc2lko', token: apitoken,  attribution: mbAttr});    
-   
+    var DarkDEM = L.tileLayer(mbStyleUrl, {id: 'brianmc29/ckvnoe41y7ebt15pelxmc2lko', token: apitoken,  attribution: mbAttr});    
     
+    
+    var outdoors = L.tileLayer(mbUrl, {id: 'mapbox.outdoors', token: apitoken,  attribution: mbAttr});
+    
+          
     //create the map*/
     var map = L.map('map', {
         center: [31.00, -5],//Coordinated to center the map
         zoom: 6, //zoom level
-        layers:darkTerrain //default base
+        layers: DarkDEM
     });
     
-    //create the basemap control layer*/
+     //create the basemap control layer*/
     var baseLayers = {
 		"Outdoors": outdoors,
-        "Grayscale": grayscale,
-		"Darkscale": dark,
-        "Dark Terrain": darkTerrain
-    };
-    
-    L.control.layers(baseLayers).addTo(map);
-};
+                
 
+    }
+    
 //calling the funcation
-$(document).ready(createMap);
+$(document).ready(function);
